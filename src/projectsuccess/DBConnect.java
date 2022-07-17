c9e2c4d7f7782317cebee53448777191aa74e70e
Pasindu Charith <hgpcsagara@gmail.com>
2022-07-18 02:45:08 +0530

    initial commit

---
+++ src/projectsuccess/DBConnect.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectsuccess;
+
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+/**
+ *
+ * @author pc
+ */
+public class DBConnect {
+   
+    
+    public static Connection connect(){
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/successdb","root","");
+            return conn;
+            
+        }
+        catch(ClassNotFoundException | SQLException e)
+                {
+            System.out.println(e);
+        }
+        return null;
+    
+    }   
+    
+}
+    
+    
---
+++ src/projectsuccess/ProjectSuccess.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectsuccess;
+
+/**
+ *
+ * @author pc
+ */
+public class ProjectSuccess {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //show home window
+        Login lg= new Login(); 
+        lg.setVisible(true);
+    }
+    
+}
